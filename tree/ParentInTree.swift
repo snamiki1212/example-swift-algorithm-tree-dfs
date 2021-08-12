f696cb48d441f098c5ae9b76906a7b372cddb80b
snamiki1212 <snamiki1212@gmail.com>
2021-08-12 11:43:54 -0700

    Refactor logic to file

---
+++ tree/ParentInTree.swift
@@ -0,0 +1,39 @@
+//
+//  ParentInTree.swift
+//  tree
+//
+//  Created by shunnamiki on 2021/08/12.
+//
+
+import Foundation
+
+
+struct ParentInTree {
+    static func main (){
+        // input
+        let n = Int(readLine()!)!
+        var list = [[Int]](repeating: [], count: n - 1)
+        for i in 0..<n-1 {
+            list[i] = (readLine()!.split(separator: " ").map { Int($0)! })
+        }
+        var parent = [Int](repeating: -1, count: n + 1)
+
+        // run
+        parent[1] = 0
+        for set in list {
+            let node1 = set[0]
+            let node2 = set[1]
+            if parent[node1] == -1{
+                parent[node1] = node2
+            } else {
+                parent[node2] = node1
+            }
+        }
+
+        // print
+        for i in 2..<parent.count {
+            print(parent[i])
+        }
+    }
+
+}
--- tree/main.swift
+++ tree/main.swift
@@ -7,28 +7,20 @@
 
 import Foundation
 
-// input
-let n = Int(readLine()!)!
-var list = [[Int]](repeating: [], count: n - 1)
-for i in 0..<n-1 {
-    list[i] = (readLine()!.split(separator: " ").map { Int($0)! })
-}
-var parent = [Int](repeating: -1, count: n + 1)
-
-// run
-parent[1] = 0
-for set in list {
-    let node1 = set[0]
-    let node2 = set[1]
-    if parent[node1] == -1{
-        parent[node1] = node2
-    } else {
-        parent[node2] = node1
-    }
-}
-
-// print
-for i in 2..<parent.count {
-    print(parent[i])
-}
+//ParentInTree.main()
+////12
+////1 2
+////1 3
+////2 4
+////3 5
+////3 6
+////4 7
+////4 8
+////5 9
+////5 10
+////6 11
+////6 12
+//
+//print("------------")
+//--------------------------------------------
 
