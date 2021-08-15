97d4205c78141f5d459eb8cd03ee5660c2f3c77b
snamiki1212 <snamiki1212@gmail.com>
2021-08-14 19:59:43 -0700

    Update logic

--- tree/ParentInTree.swift
+++ tree/ParentInTree.swift
@@ -10,30 +10,36 @@ import Foundation
 
 struct ParentInTree {
     static func main (){
-        // input
+        // get inputs to save adj list
         let n = Int(readLine()!)!
-        var list = [[Int]](repeating: [], count: n - 1)
-        for i in 0..<n-1 {
-            list[i] = (readLine()!.split(separator: " ").map { Int($0)! })
+        var adj = [[Int]](repeating: [], count: n + 1)
+        for _ in 0..<n-1 {
+            let set = (readLine()!.split(separator: " ").map { Int($0)! })
+            let n1 = set[0]
+            let n2 = set[1]
+            adj[n1].append(n2)
+            adj[n2].append(n1)
         }
-        var parent = [Int](repeating: -1, count: n + 1)
+        
+        var parents = [Int](repeating: -1, count: n + 1)
+        var visited = [Bool](repeating: false, count: n + 1)
 
         // run
-        parent[1] = 0
-        for set in list {
-            let node1 = set[0]
-            let node2 = set[1]
-            if parent[node1] == -1{
-                parent[node1] = node2
-            } else {
-                parent[node2] = node1
-            }
-        }
+        dfs(from: 1, adj: &adj, visited: &visited, parents: &parents)
 
         // print
-        for i in 2..<parent.count {
-            print(parent[i])
+        for i in 2..<parents.count {
+            print(parents[i])
         }
     }
-
+    
+    static func dfs(from: Int, adj: inout [[Int]], visited: inout [Bool], parents: inout [Int]){
+        for to in adj[from] {
+            if !visited[to] {
+                visited[to] = true
+                parents[to] = from
+                dfs(from: to, adj: &adj, visited: &visited, parents: &parents)
+            }
+        }
+    }
 }
